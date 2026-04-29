6117952e472b182bfa6d32ce8c2a3aa383b84bfe
Shruti-Soni14 <128928352+Shruti-Soni14@users.noreply.github.com>
2026-04-29 19:02:53 +0530

    Delete backend/src/CartRepository.java

--- backend/src/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.securemultivendorecommercemarketplaceplatform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import com.example.securemultivendorecommercemarketplaceplatform.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    List<Cart> findByUserId(Long userId);
-}
