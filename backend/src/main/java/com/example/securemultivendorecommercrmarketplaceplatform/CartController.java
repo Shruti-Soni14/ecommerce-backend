11db7c5980814b7ff64eba141bbf323c6ab18385
Shruti-Soni14 <128928352+Shruti-Soni14@users.noreply.github.com>
2026-05-01 17:38:53 +0530

    Update CartController.java

--- backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/CartController.java
+++ backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/CartController.java
@@ -4,26 +4,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/cart")   
 @CrossOrigin(origins = "*")
 public class CartController {
 
     private List<Map<String, Object>> cart = new ArrayList<>();
 
-    //  ADD TO CART
     @PostMapping
     public Map<String, Object> addToCart(@RequestBody Map<String, Object> item) {
-
         item.put("id", cart.size() + 1);
         cart.add(item);
-
         return item;
     }
 
-    //  GET USER CART
     @GetMapping("/{userId}")
     public List<Map<String, Object>> getCart(@PathVariable Long userId) {
-
         List<Map<String, Object>> result = new ArrayList<>();
 
         for (Map<String, Object> c : cart) {
@@ -31,14 +26,11 @@ public class CartController {
                 result.add(c);
             }
         }
-
         return result;
     }
 
-    //  DELETE ITEM
     @DeleteMapping("/{id}")
     public void removeFromCart(@PathVariable Long id) {
-
         cart.removeIf(c -> c.get("id").equals(id));
     }
 }
