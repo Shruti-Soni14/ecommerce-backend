7bbc6b18bcac47958c2d25e22ac8c928f29e7a6a
Shruti-Soni14 <128928352+Shruti-Soni14@users.noreply.github.com>
2026-05-03 13:15:31 +0530

    Update ProductRepository.java

--- backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/ProductRepository.java
+++ backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/ProductRepository.java
@@ -1,13 +1,7 @@
-package com.example.securemultivendorecommercemarketplaceplatform.repository; 
+package com.example.securemultivendorecommercemarketplaceplatform.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository; 
-import com.example.securemultivendorecommercemarketplaceplatform.model.Product; 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.securemultivendorecommercemarketplaceplatform.model.Product;
 
-import java.util.List; 
-
-public interface ProductRepository extends JpaRepository<Product, Long> { 
-    
-    
-    List<Product> findByUserId(Long userId);
-    boolean existsByName(String name); 
-} 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
