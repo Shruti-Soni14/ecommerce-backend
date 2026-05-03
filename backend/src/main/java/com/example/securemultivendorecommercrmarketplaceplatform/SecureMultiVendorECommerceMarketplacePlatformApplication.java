028a33a4c17bd16b5ed2b16cd70989d15ac84dc5
Shruti-Soni14 <128928352+Shruti-Soni14@users.noreply.github.com>
2026-05-03 13:21:17 +0530

    Update SecureMultiVendorECommerceMarketplacePlatformApplication.java

--- backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/SecureMultiVendorECommerceMarketplacePlatformApplication.java
+++ backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/SecureMultiVendorECommerceMarketplacePlatformApplication.java
@@ -2,13 +2,12 @@ package com.example.securemultivendorecommercemarketplaceplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(
-    scanBasePackages = "com.example.securemultivendorecommercemarketplaceplatform",
-    exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-    }
-)
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.securemultivendorecommercemarketplaceplatform.repository")
+@EntityScan(basePackages = "com.example.securemultivendorecommercemarketplaceplatform.model")
 public class SecureMultiVendorECommerceMarketplacePlatformApplication {
 
     public static void main(String[] args) {
