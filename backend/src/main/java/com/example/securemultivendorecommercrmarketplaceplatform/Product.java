bc658b103331dadf899d40c692baeecf6fb7a29b
Shruti-Soni14 <128928352+Shruti-Soni14@users.noreply.github.com>
2026-05-03 12:07:53 +0530

    Update Product.java

--- backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/Product.java
+++ backend/src/main/java/com/example/securemultivendorecommercrmarketplaceplatform/Product.java
@@ -1,40 +1,24 @@
 package com.example.securemultivendorecommercemarketplaceplatform.model;
-
 import jakarta.persistence.*;
-
-@Entity
-@Table(name = "products")
+@Entity 
 public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
-
     private String name;
     private double price;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id") 
+    private User user;
+    // getters setters public Long getId() { return id; }public void setId(Long id) { this.id = id; }
 
-    //  getters setters
-    public Long getId() {
-        return id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 }
